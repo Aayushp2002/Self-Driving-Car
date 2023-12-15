59f32b5f980e2c0b518e0ea2cb3d64ee5326e195
Aayush Patel <hd3998@wayne.edu>
2023-12-15 11:39:04 -0500

    Completed framework of road

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ const canvas=document.getElementById("myCanvas"); // get the canvas element
 canvas.width=200; // set canvas width to 200px
 
 const ctx=canvas.getContext("2d"); // get the context of the canvas
-const car=new Car(100,100,30,50); // create a new car object
+const road=new Road(canvas.width/2,canvas.width*0.9); // create a new road object
+const car=new Car(road.getLaneCenter(1),100,30,50); // create a new car object
 car.draw(ctx); // draw the car
 animate();
 
 function animate(){ // animate the car
     car.update(); // update the car
     canvas.height=window.innerHeight; // set canvas height to window height
+    ctx.save(); // save the context
+    ctx.translate(0,-car.y+canvas.height*(7/10)); // translate the context
+
+    road.draw(ctx); // draw the road
     car.draw(ctx); // draw the car
+    ctx.restore(); // restore the context
     requestAnimationFrame(animate); // animate the car
 }
---
+++ road.js
@@ -0,0 +1,53 @@
+class Road{ // Road class
+    constructor(x,width,laneCount=3){ // constructor takes in x,width,laneCount
+        this.x=x;
+        this.width=width;
+        this.laneCount=laneCount; // set the lane count
+
+        this.left=this.x-this.width/2;
+        this.right=this.x+this.width/2;
+        const infinity=1000000; // set infinity to 1000000
+        this.top=-infinity; // set the top to negative infinity
+        this.bottom=infinity; // set the bottom to infinity
+
+        const topLeft=[ this.left,this.top]; // top left corner
+        const topRight=[this.right,this.top]; // top right corner
+        const bottomLeft=[this.left,this.bottom]; // bottom left corner
+        const bottomRight=[this.right,this.bottom]; // bottom right corner
+        this.borders=[
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ];
+    }
+
+    getLaneCenter(laneIndex){ // get the center of the lane
+        const laneWidth=this.width/this.laneCount; // get the width of the lane
+        return this.left+laneWidth/2+Math.min(laneIndex,this.laneCount-1)*laneWidth; // return the center of the lane
+    }
+
+    draw(ctx){ // draw the road
+        ctx.lineWidth=5;
+        ctx.strokeStyle="white";
+
+        for(let i=1;i<=this.laneCount-1;i++){ // for each lane
+            const x=lerp( // linear interpolation to get the x value of the lane line
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            ctx.setLineDash([20, 20]);
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
+
+        ctx.setLineDash([]);
+        this.borders.forEach((border)=>{ // for each border
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        });
+    }
+}
