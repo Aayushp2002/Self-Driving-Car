bf05486ea26e80691764165a3dbe6b0bcad4ad07
Aayush Patel <hd3998@wayne.edu>
2023-12-18 13:20:51 -0500

    Added neural network visualizer so output does not have to be explained

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const canvas=document.getElementById("myCanvas"); // get the canvas element
+const carCanvas=document.getElementById("carCanvas"); // get the canvas element
+carCanvas.width=200; // set canvas width to 200px
+const networkCanvas=document.getElementById("networkCanvas"); // get the canvas element
+networkCanvas.width=300; // set canvas width to 200px
 
-canvas.width=200; // set canvas width to 200px
+const carCtx=carCanvas.getContext("2d"); // get the context of the canvas
+const networkCtx=networkCanvas.getContext("2d"); // get the context of the canvas
 
-const ctx=canvas.getContext("2d"); // get the context of the canvas
-const road=new Road(canvas.width/2,canvas.width*0.9); // create a new road object
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9); // create a new road object
 const car=new Car(road.getLaneCenter(1),100,30,50,"AI"); // create a new car object
 // car.draw(ctx); // draw the car
 const traffic=[
@@ -11,20 +14,24 @@ const traffic=[
 ];
 animate();
 
-function animate(){ // animate the car
+function animate(time){ // animate the car
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
     car.update(road.borders,traffic); // update the car
-    canvas.height=window.innerHeight; // set canvas height to window height
-    ctx.save(); // save the context
-    ctx.translate(0,-car.y+canvas.height*(7/10)); // translate the context
+    carCanvas.height=window.innerHeight; // set canvas height to window height
+    networkCanvas.height=window.innerHeight; // set canvas height to window height
+    carCtx.save(); // save the context
+    carCtx.translate(0,-car.y+carCanvas.height*(7/10)); // translate the context
 
-    road.draw(ctx); // draw the road
+    road.draw(carCtx); // draw the road
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(ctx,"red");
+        traffic[i].draw(carCtx,"red");
     }
-    car.draw(ctx,"blue"); // draw the car
-    ctx.restore(); // restore the context
+    car.draw(carCtx,"blue"); // draw the car
+    carCtx.restore(); // restore the context
+
+    networkCtx.lineDashOffset=-time/50; // set the line dash offset
+    Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate); // animate the car
 }
--- utils.js
+++ utils.js
@@ -42,6 +42,14 @@ function polysIntersect(poly1, poly2){
     return false;
 }
 
+function getRGBA(value){
+    const alpha=Math.abs(value);
+    const R=value<0?0:255;
+    const G=R;
+    const B=value>0?0:255;
+    return "rgba("+R+","+G+","+B+","+alpha+")";
+}
+
 // // Test cases
 // function testGetIntersection() {
 //     // Example of intersecting lines
